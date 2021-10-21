9d1cb61a95571eedd23cbdaac521b78cc88e1cc4
Dyupich <warrior2001@mail.ru>
2021-10-21 14:11:52 +0300

    Добавление свойств навигации и внешних ключей.

---
+++ OOP_ASU_5.Domain/Exercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ASU_5.Domain
+{
+    public class Exercise
+    {
+
+        // Properties
+        public Guid Id { get; set; }
+        public string ExerciseType { get; set; }
+        public string Number { get; set; }
+        public double DifficultyCoefficient { get; set;}
+
+        // Foreign keys
+        public Guid ExercisesBlockId { get; set; }
+        public Guid ContentLinkId { get; set; }
+
+        // Navigation properites
+        public ExercisesBlock ExercisesBlock { get; set; }
+        public ContentLink ContentLink { get; set; }
+        //Ссылка на тест - добавить
+        public List<ExerciseVariant> ExercisesVariants { get; set; }
+
+    }
+}
---
+++ OOP_ASU_5.Domain/Option.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ASU_5.Domain
+{
+    public class Option
+    {
+        // Properties
+        public Guid Id { get; set; }
+        public string OptionType { get; set; }
+        public string Number { get; set; }
+        public string Description { get; set; }
+        public Boolean FreeFormat { get; set; }
+
+        // Foreign keys
+        public Guid ExerciseId { get; set; }
+
+        // Navigation properites
+        public Exercise Exercise { get; set; }
+    }
+}
